c26d67da916a16fe384f5f1011698fe2d14e2ad7
d-plaindoux <d.plaindoux@free.fr>
2026-06-16 08:11:32 +0200

    Refactoring

--- test/zigma_nominal_test.zig
+++ test/zigma_nominal_test.zig
@@ -13,13 +13,30 @@ const Int = struct {
     }
 };
 
-test "should Modularize IntAdd and call add" {
+const Float = struct {
+    pub inline fn add(a: f64, b: f64) f64 {
+        return a + b;
+    }
+};
+
+test "should Modularize Int and call add" {
     // Given
-    const IntNumber = zigma.implement(Number(u32)).with(Int);
+    const IntNumber = zigma.core.implement(Number(u32)).with(Int);
 
     // When
     const value = IntNumber.add(1, 2);
 
     // Then
     try std.testing.expectEqual(3, value);
+}
+
+test "should Modularize Float and call add" {
+    // Given
+    const IntNumber = zigma.core.implement(Number(f64)).with(Float);
+
+    // When
+    const value = IntNumber.add(1.5, 2.5);
+
+    // Then
+    try std.testing.expectEqual(4.0, value);
 }
